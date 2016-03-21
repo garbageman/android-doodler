8d5a248cd35075b3dcf7fbc42218385fe118b891
garbageman <dnagle@umd.edu>
2016-03-20 20:07:49 -0400

    Added a tool box that minimizes

--- app/src/main/java/org/hci/doodler/DoodleView.java
+++ app/src/main/java/org/hci/doodler/DoodleView.java
@@ -1,48 +1,76 @@
 package org.hci.doodler;
 
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.content.Context;
+
+import java.util.HashMap;
+import java.util.LinkedList;
 
 /**
  * Created by damiennagle on 3/9/16.
  */
 public class DoodleView extends View {
 
+    private static float paintBrushMax = 20.0f;
     private Paint paintDoodle = new Paint();
-    private Path path = new Path();
 
-    public DoodleView(Context context){
+    private HashMap<Path,Paint> drawnPaths = new HashMap<Path,Paint>();
+    private LinkedList<Path> pathOrder = new LinkedList<Path>();
+
+    private Path currentPath;
+    private Paint currentPaint;
+
+    private float currentStrokeWidth;
+
+    /* Paint color variables */
+    private int currentOpacity;
+    private int currR;
+    private int currB;
+    private int currG;
+
+    public DoodleView(Context context) {
         super(context);
         init(null,0);
     }
 
     public DoodleView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(attrs,defStyleAttr);
+        init(attrs, defStyleAttr);
     }
 
     public DoodleView(Context context, AttributeSet attrs){
-        super(context,attrs);
-        init(attrs,0);
+        super(context, attrs);
+        init(attrs, 0);
     }
 
     private void init(AttributeSet attrs, int defStyle){
-        paintDoodle.setColor(Color.RED);
-        paintDoodle.setAntiAlias(true);
-        paintDoodle.setStyle(Paint.Style.STROKE);
+        /* Initialize current variables */
+        this.currentStrokeWidth = 6.0f;
+        this.currentOpacity = 255;
+
+        /* Default color is black */
+        this.currR = 0;
+        this.currB = 0;
+        this.currG = 0;
+
     }
 
     @Override
     public void onDraw(Canvas canvas){
         super.onDraw(canvas);
 
-        canvas.drawPath(path, paintDoodle);
+        /* Iterate over the map and draw all the paths with their paints */
+        for (Path p : drawnPaths.keySet()) {
+            Paint currPaint = drawnPaths.get(p);
+            canvas.drawPath(p, currPaint);
+        }
+
     }
 
     @Override
@@ -51,11 +79,12 @@ public class DoodleView extends View {
         float touchY = motionEvent.getY();
 
         switch(motionEvent.getAction()){
+            /* This is going to create a new path */
             case MotionEvent.ACTION_DOWN:
-                path.moveTo(touchX,touchY);
+                this.downPath(touchX, touchY);
                 break;
             case MotionEvent.ACTION_MOVE:
-                path.lineTo(touchX,touchY);
+                this.currentPath.lineTo(touchX, touchY);
                 break;
             case MotionEvent.ACTION_UP:
                 break;
@@ -63,6 +92,132 @@ public class DoodleView extends View {
 
         invalidate();
         return true;
+    }
+
+    /**
+     * This will initialize a new path at this location,
+     * or continue the current path if the paint parameters haven't changed
+     * @param touchX
+     * @param touchY
+     */
+    private void downPath(float touchX, float touchY) {
+        if (changedParameters()) {
+
+            /* If the paint parameters have changed we must make a new path */
+            Path newPath = new Path();
+            newPath.moveTo(touchX, touchY);
+
+            /* Create a new paint from the parameters */
+            Paint newPaint = createPaint();
+
+            /* Save the path */
+            this.pathOrder.add(newPath);
+            this.drawnPaths.put(newPath, newPaint);
+
+            /* Set the new path and color */
+            this.currentPath = newPath;
+            this.currentPaint = newPaint;
+
+        } else {
+
+            /* Continue the current path at a new place */
+            this.currentPath.moveTo(touchX,touchY);
+
+        }
+    }
+
+    private boolean changedParameters() {
+        /* If current paint is null reinitialize it */
+        if (currentPaint == null || currentPath == null) {
+            return true;
+        }
+
+        /* Check if any of the parameters are different from the current paint type */
+        if (currentPaint.getColor() != Color.argb(this.currentOpacity,this.currR,this.currG,this.currB)) {
+            return true;
+        }
+
+        /* Not sure if we need error correction for floats */
+        if (Math.abs(currentPaint.getStrokeWidth() - currentStrokeWidth) > 0.05f ) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private Paint createPaint() {
+        Paint newPaint = new Paint();
+        /* Create a color from current values */
+        newPaint.setStrokeWidth(this.currentStrokeWidth);
+        newPaint.setColor(Color.argb(this.currentOpacity,this.currR,this.currG,this.currB));
+        newPaint.setStyle(Paint.Style.STROKE);
+        newPaint.setAntiAlias(true);
+
+        return newPaint;
+    }
+
+    /**
+     * Takes in a size from 0 - 100 and sets the tooltip to that size scaled to the min and max of the drawing tool
+     * @param size
+     */
+    public void setToolTipSize(int size) {
+        float percentage = (float) (size + 1) / 10.0f;
+        currentStrokeWidth = percentage * paintBrushMax;
+    }
+
+    /**
+     * The bar that controls the opacity goes from 0 - 255 so no need to convert
+     * @param opacity
+     */
+    public void setToolTipOpacity(int opacity) {
+        currentOpacity = opacity;
+    }
+
+    /**
+     * This should clear all of the
+     */
+    public void clearScreen() {
+        /* Set all the paints as white */
+        for(Path p : drawnPaths.keySet()) {
+            drawnPaths.get(p).setColor(Color.WHITE);
+        }
+
+        /* Force a repaint */
+        invalidate();
+
+        /* Remove the old paths */
+        drawnPaths = new HashMap<Path,Paint>();
+    }
+
+    /* This updates the value for the color when a button has been pressed */
+    public void updateColor(String buttonVal) {
+        System.out.println("Updating value to: " + buttonVal);
+
+        switch (buttonVal) {
+            case "Red":
+                this.currR = 255;
+                this.currG = 0;
+                this.currB = 0;
+                break;
+            case "Green":
+                this.currR = 0;
+                this.currG = 255;
+                this.currB = 0;
+                break;
+            case "Blue":
+                this.currR = 0;
+                this.currG = 0;
+                this.currB = 255;
+                break;
+            case "Black":
+                this.currR = 0;
+                this.currG = 0;
+                this.currB = 0;
+                break;
+            case "Clear Screen":
+                this.clearScreen();
+                break;
+        }
     }
 
 }
--- app/src/main/java/org/hci/doodler/MainActivity.java
+++ app/src/main/java/org/hci/doodler/MainActivity.java
@@ -8,12 +8,101 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SeekBar toolTipSize = null;
+    private SeekBar opacity = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Set the tool tip size seek bar */
+
+        toolTipSize = (SeekBar) findViewById(R.id.toolTipSize);
+
+        toolTipSize.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChanged = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                /* Set the new value of the toolTipSize */
+                DoodleView doodleView = (DoodleView) findViewById(R.id.doodleView);
+                doodleView.setToolTipSize(progressChanged);
+            }
+        });
+
+        /* Create the opacity seekbar */
+
+        opacity = (SeekBar) findViewById(R.id.opacity);
+
+        opacity.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChanged = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                /* Set the new value of the toolTipSize */
+                DoodleView doodleView = (DoodleView) findViewById(R.id.doodleView);
+                doodleView.setToolTipOpacity(progressChanged);
+            }
+        });
+
+        LinearLayout toolboxGrid = (LinearLayout) findViewById(R.id.buttonLayout);
+
+        ButtonClickHandler buttonClickHandler = new ButtonClickHandler();
+
+        for (int i = 0; i < toolboxGrid.getChildCount(); i++) {
+            View v = toolboxGrid.getChildAt(i);
+            if (v instanceof Button) {
+                v.setOnClickListener(buttonClickHandler);
+            }
+        }
+
+        /* Add the clear screen button to the click listener */
+        Button clearScreen = (Button) findViewById(R.id.clearButton);
+
+        clearScreen.setOnClickListener(buttonClickHandler);
+
+        /* Set TextView Values */
+        TextView widthLabel = (TextView) findViewById(R.id.toolTipSizeLabel);
+        widthLabel.setText("Tooltip Size: Small -> Large");
+
+        TextView opacityLabel = (TextView) findViewById(R.id.opacityLabel);
+        opacityLabel.setText("Tooltip Opacity: Clear -> Solid");
+    }
+
+    private class ButtonClickHandler implements View.OnClickListener {
+
+        /**
+         *  This will handle the buttons being clicked to change the color of the app or clear the screen
+         * @param v
+         */
+        @Override
+        public void onClick(View v) {
+            DoodleView doodleView = (DoodleView) findViewById(R.id.doodleView);
+            doodleView.updateColor(((Button) v).getText().toString());
+        }
     }
 }
